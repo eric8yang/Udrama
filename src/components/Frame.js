9f7d7dc0af8a3bd8a9829d14d1c98517ed66a76e
eric8yang <32950723+eric8yang@users.noreply.github.com>
2020-12-28 18:05:52 -0800

    added star ratings
    made star and filter stay after rating
    lit up stars

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -1,13 +1,45 @@
 import './Frame.css';
 import React from 'react';
+import grayStar from '../images/starGray.png';
+import yellowStar from '../images/starYellow.png';
+
+
 
 function Frame(props){
+    const lightUpStars = (starNumber, name) => {
+        for (let i = 1; i <= 5; i++){
+            var elem = document.getElementById("star" + i + name);
+            elem.classList.remove("yellowStar");
+            elem.classList.remove("grayStar");
+        }
+
+        for (let i = 1; i <= 5; i++){
+            if (i <= starNumber) {
+                var elem = document.getElementById("star" + i + name);   
+                elem.classList.add("yellowStar");
+            }
+            else {
+                var elem = document.getElementById("star" + i + name);
+                elem.classList.add("grayStar");
+            }
+        }
+
+        var rateElem = document.getElementById("rate" + name);
+        rateElem.classList.add("rateStatic");
+    }
+
     return <div className = "container"> 
         <div className = "imageViewer">
             <img className = "image" src ={props.image} alt = {props.name}>
             </img>
-            <div className = "rate">
-                <div className = "stars"></div>
+            <div className = "rate" id = {"rate" + props.name}>
+                <div className = "starContainer">
+                    <button className = "star" id = {"star1" + props.name} onClick = {() => lightUpStars("1", props.name)}/>
+                    <button className = "star" id = {"star2" + props.name} onClick = {() => lightUpStars("2", props.name)}/>
+                    <button className = "star" id = {"star3" + props.name} onClick = {() => lightUpStars("3", props.name)}/>
+                    <button className = "star" id = {"star4" + props.name} onClick = {() => lightUpStars("4", props.name)}/>
+                    <button className = "star" id = {"star5" + props.name} onClick = {() => lightUpStars("5", props.name)}/>
+                </div> 
             </div>
         </div>
         <div className = "title">
